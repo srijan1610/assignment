3e4ce84a585b3a1beb1c60bb7c81d609db1645b8
srijan1610 <srijanraj23@gmail.com>
2018-06-07 14:08:39 +0530

    assignment 3

---
+++ assignment3.py
@@ -0,0 +1,32 @@
+#l=[]
+#a=input("enter no")
+#l.append(a)
+#print(l)
+
+#q2
+#b=input("enter the string")
+#l.append(b)
+#print(l)
+
+#q3
+l=["apple","microsoft","apple","tesla"]
+print(l.count("micrsoft"))
+
+#q4
+l=[2,3,4,6,7]
+l.sort()
+print(l)
+
+#q5
+a=[1,3,5,7,9]
+b=[2,4,6,8,10]
+c=a+b
+c.sort()
+print(c)
+
+#q6
+l=[1,2,3,4,5]
+l.append(6)
+print(l)
+l.pop()
+print(1)
