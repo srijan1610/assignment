f9cd69f7869c98892c4f63ef73d81a3328515c76
srijan1610 <39923458+srijan1610@users.noreply.github.com>
2018-06-23 11:47:50 +0530

    Update filehandlings.py

--- filehandlings.py
+++ filehandlings.py
@@ -1,46 +1,47 @@
-'''
+
 #Q1
-f=open("file.txt",encoding="utf8")
+f=open("test1.txt",encoding="utf8")
 a=(f.readlines())
 a.reverse()
 n=int(input("enter number of lines u want to print"))
 for i in range(0,n):
     print(a[i])
 f.close()
-'''
+
 
 #Q2
-'''
-a="kill"
-f=open("file4.txt","r")
-k=f.read()
-m=k.split()
-print(k.count(a))
-'''
+
+a="love"
+f=open("test4.txt","r")
+z=f.read()
+m=z.split()
+print(z.count(a))
+
 #Q3
-'''
-f=open("file.txt",encoding="utf8")
+
+f=open("test.txt",encoding="utf8")
 a=(f.readlines())
-c=open("file2.txt","w")
+c=open("test2.txt","w")
 c.writelines(a)
-'''
+
 
 #Q4
-'''
+
 i=0
-f=open("file2.txt",encoding="utf8")
+f=open("test2.txt",encoding="utf8")
 a=(f.readlines())
-b=open("file4.txt",encoding="utf8")
+b=open("test4.txt",encoding="utf8")
 c=(b.readlines())
-d= open("file3.txt","w")
+d= open("test3.txt","w")
 for i,j in zip(a,c):
     d.write(i+j)
 f.close()
 b.close()
 d.close()
-'''
+
 
 #q5
+
 import random
 obj1=open("test.txt","w")
 obj2=open("sorted1.txt","w")
